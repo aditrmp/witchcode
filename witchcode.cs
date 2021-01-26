853a78d59a1a1c4238a5ad9df84ec60c8b2788cf
aditrmp <58688383+aditrmp@users.noreply.github.com>
2021-01-26 23:48:48 +0700

    Update witchcode.cs

--- witchcode.cs
+++ witchcode.cs
@@ -19,46 +19,46 @@ public class Program
 	}
 }
 
-    class Result{
-		public static double countAverage(int vAgeFirst, int vYodFirst, int vAgeSec, int vYodSec){
-			double result = 0;
-			int totalVictims1 = 0;
-			int totalVictims2 = 0;
-			int yobFirst = vYodFirst-vAgeFirst;
-			int yobSec = vYodSec-vAgeSec;
+class Result{
+	public static double countAverage(int vAgeFirst, int vYodFirst, int vAgeSec, int vYodSec){
+		double result = 0;
+		int totalVictims1 = 0;
+		int totalVictims2 = 0;
+		int yobFirst = vYodFirst-vAgeFirst;
+		int yobSec = vYodSec-vAgeSec;
 			
-			if(yobFirst < 0 || yobSec < 0){
-				result = -1;	
-			} else{
-				totalVictims1 = countPeopleKilled(yobFirst);
-				totalVictims2 = countPeopleKilled(yobSec);
-				result =  (totalVictims1+totalVictims2)/2;
-				Console.WriteLine("korban1 "+countPeopleKilled(yobFirst));
-				Console.WriteLine("korban2 "+countPeopleKilled(yobSec));
-			}
-			
-			return result;
-    	}
-		
-		public static int countPeopleKilled (int year){
-			int num1 = 0;
-			int num2 = 1;
-			int num3 = 0;
-			int result = 0;
-			Console.WriteLine(year);
-			
-			if(year == 1){
-				result = 1;
-			} else if(year > 1){
-				result = 1;
-				for(int i=1; i<year; i++){
-					num3 = num1 + num2;
-					Console.Write(num3 + ", ");
-					result += num3;
-					num1 = num2;
-					num2 = num3;
-				}
-			}
-			return result;
+		if(yobFirst < 0 || yobSec < 0){
+			result = -1;	
+		} else{
+			totalVictims1 = countPeopleKilled(yobFirst);
+			totalVictims2 = countPeopleKilled(yobSec);
+			result =  (totalVictims1+totalVictims2)/2;
+			Console.WriteLine("korban1 "+countPeopleKilled(yobFirst));
+			Console.WriteLine("korban2 "+countPeopleKilled(yobSec));
 		}
+			
+		return result;
+   	}
+		
+	public static int countPeopleKilled (int year){
+		int num1 = 0;
+		int num2 = 1;
+		int num3 = 0;
+		int result = 0;
+		Console.WriteLine(year);
+			
+		if(year == 1){
+			result = 1;
+		} else if(year > 1){
+			result = 1;
+			for(int i=1; i<year; i++){
+				num3 = num1 + num2;
+				Console.Write(num3 + ", ");
+				result += num3;
+				num1 = num2;
+				num2 = num3;
+			}
+		}
+		return result;
 	}
+}
